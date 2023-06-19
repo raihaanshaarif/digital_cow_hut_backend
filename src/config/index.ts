5aa978d6b9a812f91f5b1b99c1baa2ce426585e9
raihaanshaarif <codexbd@gmail.com>
2023-06-19 17:27:45 +0600

    error Handling Done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), ".env") }); //cwd is the current working directory
+dotenv.config({ path: path.join(process.cwd(), '.env') }); //cwd is the current working directory
 
 export default {
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-}
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  default_user_pass: process.env.DEFAULT_USER_PASS,
+};
